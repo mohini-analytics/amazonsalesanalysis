ec5c84db463c568a5f344276da06078162331356
Mohini Rana <MOHINIRANA1207@GMAIL.COM>
2025-06-06 20:05:51 +0100

    Update findings.sql

--- findings.sql
+++ findings.sql
@@ -1,4 +1,4 @@
--- Business Questions:
+-- Potential Business Questions:
 
 -- 1) How many total products are listed?
 
@@ -27,5 +27,89 @@ GROUP BY categoryName
 ORDER BY product_count DESC
 LIMIT 10
 
--- 5) Top 10 categories have the most average price?
+-- 5) Top 10 categories have the highest average price?
+
+SELECT categoryName, ROUND(AVG(price), 2) AS avg_price, COUNT(*) AS total_products
+FROM `fresh-thinker-451616-g2.amazonUK.amazon_data` 
+WHERE price IS NOT NULL AND categoryName IS NOT NULL
+GROUP BY categoryName
+ORDER BY avg_price DESC
+LIMIT 10;
+
+-- 6) Top 10 categories have the highest average price?
+
+SELECT categoryName, ROUND(AVG(price), 2) AS avg_price, COUNT(*) AS total_products
+FROM `fresh-thinker-451616-g2.amazonUK.amazon_data` 
+WHERE price IS NOT NULL AND categoryName IS NOT NULL
+GROUP BY categoryName
+ORDER BY avg_price 
+LIMIT 10;
+
+-- 7) Which categories have the most products with ratings?
+
+SELECT categoryName, COUNT(*) AS rated_products
+FROM `fresh-thinker-451616-g2.amazonUK.amazon_data`
+WHERE stars IS NOT NULL
+GROUP BY categoryName
+ORDER BY rated_products DESC
+LIMIT 10;
+
+-- 8) How many products are missing price or rating information?
+
+SELECT 
+  COUNTIF(price IS NULL) AS missing_price,
+  COUNTIF(stars IS NULL) AS missing_rating,
+  COUNTIF(reviews IS NULL) AS missing_reviews
+FROM `fresh-thinker-451616-g2.amazonUK.amazon_data`
+
+-- 9) What is the average, min, and max price across all products?
+
+SELECT 
+  ROUND(AVG(price), 2) AS avg_price,
+  MIN(price) AS min_price,
+  MAX(price) AS max_price
+FROM `fresh-thinker-451616-g2.amazonUK.amazon_data`
+WHERE price IS NOT NULL;
+
+-- 10) Is there a relationship between price and rating?
+
+SELECT 
+  ROUND(CORR(price, stars), 3) AS price_rating_correlation
+FROM `fresh-thinker-451616-g2.amazonUK.amazon_data`
+WHERE price IS NOT NULL AND stars IS NOT NULL;
+
+-- 11(A) Does correlation differs by product category??
+SELECT 
+  categoryName,
+  COUNT(*) AS num_products,
+  ROUND(CORR(price, stars), 3) AS price_rating_correlation
+FROM `fresh-thinker-451616-g2.amazonUK.amazon_data`
+WHERE price IS NOT NULL AND stars IS NOT NULL
+GROUP BY categoryName
+ORDER BY price_rating_correlation 
+
+-- 11(B) Does correlation differs by product category?
+
+SELECT 
+  categoryName,
+  COUNT(*) AS num_products,
+  ROUND(CORR(price, stars), 3) AS price_rating_correlation
+FROM `fresh-thinker-451616-g2.amazonUK.amazon_data`
+WHERE price IS NOT NULL AND stars IS NOT NULL
+GROUP BY categoryName
+ORDER BY price_rating_correlation DESC
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
